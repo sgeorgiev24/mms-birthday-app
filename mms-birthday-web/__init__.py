fe16e9758f8a1fa8319829db42283ebc5a705998
Stanislav Georgiev <sgeorgiev24@gmail.com>
2020-03-01 10:29:01 +0200

    Fix code style.
    Signed-off-by: Stanislav Georgiev <sgeorgiev24@gmail.com>

--- mms-birthday-web/__init__.py
+++ mms-birthday-web/__init__.py
@@ -6,25 +6,25 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        DATABASE = os.path.join(app.instance_path, 'mms-birthday-app.sqlite')
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'mms-birthday-app.sqlite')
     )
-    
+
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-        
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     from . import db
     db.init_app(app)
-    
+
     @app.route('/')
     def hello():
         return 'Hello'
-    
+
     return app
--- mms-birthday-web/db.py
+++ mms-birthday-web/db.py
@@ -12,26 +12,30 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-        
+
     return g.db
+
 
 def close_db(e=None):
     db = g.pop('db', None)
-    
+
     if db is not None:
         db.close()
 
+
 def init_db():
     db = get_db()
-    
+
     with current_app.open_resource('schema.sql') as schema:
         db.executescript(schema.read().decode('utf8'))
+
 
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
     init_db()
     click.echo('DB is initialized :)')
+
 
 def init_app(app):
     app.teardown_appcontext(close_db)
