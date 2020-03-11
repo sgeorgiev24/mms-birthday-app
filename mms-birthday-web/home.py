a13acfb0e48fd4dadd9d254a6ea0e99dcd9baaf9
Stanislav Georgiev <sgeorgiev24@gmail.com>
2020-03-11 16:24:14 +0200

    User can no longer see it's own birthday.
    Signed-off-by: Stanislav Georgiev <sgeorgiev24@gmail.com>

--- mms-birthday-web/home.py
+++ mms-birthday-web/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
 from .db import get_db
 
@@ -9,14 +9,20 @@ bp = Blueprint('home', __name__)
 def index():
     db = get_db()
 
+    user_id = None
+
+    if g.user:
+        user_id = g.user['id']
+
     # the keys are id, current_birthday_date, name, last_name
     birthdays = db.execute(
         'select birthday.id, birthday.current_birthday_date, user.name, '
         'user.last_name '
         'from birthday '
         'join user on birthday.user_id=user.id '
-        'where is_active=1 '
-        'order by birthday.id desc'
+        'where is_active=1 and birthday.user_id != ?'
+        'order by birthday.id desc',
+        (user_id,)
     ).fetchall()
 
     return render_template('home/index.html', birthdays=birthdays)
